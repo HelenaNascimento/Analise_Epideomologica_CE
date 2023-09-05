e53f2352f7de47b807df0d8bb0e6f23a3d804f18
Helena <hnasc.1989@gmail.com>
2023-09-05 17:44:24 -0300

    'alteracao 05092023'

---
+++ Consulta_pr.sql
@@ -0,0 +1,10 @@
+--código do produto
+--descrição
+--unidade de venda
+--Fabricante
+--prec da ultima entrada
+--Prc Médio
+--Prc Venda
+--Qtd Físico
+--format(((Qtd_Fisico - (Qtd_Quaren + Qtd_Reserv)) * Prc_CusMedPra), 'c', 'pt-br') as  'Total'
+
