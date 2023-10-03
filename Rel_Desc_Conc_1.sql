88ad7e3ef61ee1a2fd668fa8f73b4da60be8fbee
Helena <hnasc.1989@gmail.com>
2023-10-03 17:37:55 -0300

    "alteração 03102023"

--- Rel_Desc_Conc_1.sql
+++ Rel_Desc_Conc_1.sql
@@ -11,11 +11,11 @@ select
     end,
     ct.Par_Documento,
     ct.[Status],
-    format(ct.Vlr_Documento, 'c', 'pt-br') as VlrDocumento,
-    format(ct.Vlr_DescConced, 'c', 'pt-br') as VlrDescontoConcedido,
-	format(IsNull(bx.Vlr_Juros, 0), 'c', 'pt-br') as VlrJuros,
+    CONVERT(DECIMAL(10,2), ct.Vlr_Documento) as VlrDocumento,
+    CONVERT(DECIMAL(10,2),ct.Vlr_DescConced) as VlrDescontoConcedido,
+	IsNull(bx.Vlr_Juros, 0) as VlrJuros,
     IsNull(bx.Qtd_DiasAtraso, 0) as Qtd_DiasAtraso,
-	format((ct.Vlr_Documento - ct.Vlr_DescConced ), 'c', 'pt-br') as '(Vlr_Doc - Vlr_DescCon)',
+	(ct.Vlr_Documento - ct.Vlr_DescConced ) as '(Vlr_Doc - Vlr_DescCon)',
 	IsNull(format(((ct.Vlr_Documento + Isnull(bx.Vlr_Juros, 0)) - (ct.Vlr_DescConced)), 'c', 'pt-br'), 0) as ValorFinal
 	from CTREC ct
 		left outer join CLIEN cl on ct.Cod_Cliente = cl.Codigo
@@ -26,8 +26,8 @@ select
 							Qtd_DiasAtraso
 						from bxrec where cod_Estabe = 1) bx on ct.cod_estabe = bx.Cod_Estabe and ct.cod_Documento = bx.cod_Documento
 where ct.cod_estabe = 1
-    and ct.Transacao >= '20220101' 
-	and ct.Transacao < '20230101' 
+    and ct.Transacao >= '20180101' 
+	and ct.Transacao < '20190101' 
 	--and ct.usuario = 'DENICE'
     and Vlr_DescConced > 0
     and ct.[Status] <> 'C'
