5fbf8e80e161d174ab34f7b3f405527f08ef0a10
Helena <hnasc.1989@gmail.com>
2023-10-08 15:36:51 -0300

    'FICHACLIENTE 08102023'

---
+++ ficha_cliente.sql
@@ -0,0 +1,27 @@
+select 
+	Dias_Atraso = cast(((getdate()-1) - Dat_Vencimento)as int),
+	format(Dat_Vencimento, 'd') as Dat_Vencimento, 
+	format(Dat_Emissao, 'd') as Dat_Emissao,
+	convert(decimal(10,2), Vlr_Documento) as Vlr_Documento,
+	convert(decimal(10,2), ct.Per_Juros) as Per_Juros,
+	convert(decimal(10,2), ((Vlr_Documento * (Per_Juros / 100)))) as Ao_Dia,
+	convert(decimal(10,2), ((Vlr_Documento * (Per_Juros / 100)) * cast(((getdate() - 1) - Dat_Vencimento)as int))) as 'Vlr_Jrs+Mult',
+	convert(decimal(10,2), ((Vlr_Documento * (Per_Juros / 100)) * cast(((getdate() - 1) - Dat_Vencimento)as int)) + Vlr_Documento) as Vlr_Atual,
+	Status as Status_Documento,
+	Num_Documento,
+	Cod_Documento,
+	ct.Par_Documento,
+	cl.Cgc_Cpf,
+	cl.Razao_Social,
+	cd.Descricao as Cidade,
+	ve.Nome_Guerra as Vendedor,
+	Cod_Agente,
+	Cod_EstOri
+from CTREC ct
+	inner join CLIEN cl on ct.Cod_Cliente = cl.Codigo
+	inner join CIDAD cd on cl.Cod_Cidade = cd.Codigo
+	inner join VENDE ve on ct.Cod_Vendedor = ve.Codigo
+where Cod_Estabe = 1
+	and ct.Status = 'A'
+	and cast(((getdate()-1) - Dat_Vencimento)as int) > 0
+--	and Cod_Documento = 666376
---
+++ ficha_cliente2.sql
@@ -0,0 +1,40 @@
+select 
+	cb.Cod_Cliente,
+	cl.Razao_Social,
+	cd.Descricao,
+	ct.Num_Documento,
+	cb.num_nota,
+	FORMAT(cb.Dat_Emissao, 'd') as Dat_Emissao,
+	FORMAT(ct.dat_vencimento, 'd') as Dat_vencimento,
+	IsNull(Format(ct.dat_quitacao, 'd'), 'N�oPago') as dat_quitacao,
+	Dias_Atraso =  case
+		when cast((dat_quitacao - Dat_vencimento)as int) is not null then cast((dat_quitacao - Dat_vencimento)as int)
+		when cast((dat_quitacao - Dat_vencimento)as int) is null then cast((getdate() - Dat_vencimento)as int)
+	end,
+	count(num_nota) as qtd_num_nota,
+	FORMAT(sum(Vlr_TotalNota), 'c', 'pt-br') as Vlr_TotalNota
+	from NFSCB CB
+		inner join CTREC CT ON cb.Cod_Estabe = ct.Cod_Estabe and cb.Cod_Pedido = ct.Cod_Pedido  
+		inner join CLIEN CL ON cb.Cod_Cliente = cl.codigo and ct.Cod_Cliente = cl.Codigo
+		left join CIDAD CD ON cl.Cod_Cidade = cd.Codigo
+where cb.Cod_Estabe = 1 
+	and Ser_Nota = '1'
+	and Tip_Saida = 'V'
+	and Ret_CStat = 100
+
+
+group by 
+	ct.Num_Documento,
+	cb.num_nota,
+	cb.Cod_Cliente,
+	cl.Razao_Social,
+	cd.Descricao,
+	cb.Dat_Emissao,
+	ct.dat_vencimento,
+	ct.dat_quitacao
+
+
+--Dias_Atraso = cast(((getdate()-1) - Dat_Vencimento)as int)
+
+
+--SELECT TOP 5 * FROM CTREC
