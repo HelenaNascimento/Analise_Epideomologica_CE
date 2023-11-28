7f7bd17feec650e7b5ca9e7b5e2b2ee2f7fbe4b0
HELENA <hnasc.1989@gmail.com>
2023-11-27 22:16:55 -0300

    'atualização 2711'

--- AnaEntFabABC.sql
+++ AnaEntFabABC.sql
@@ -21,7 +21,7 @@ select
 		inner join NFEIT IT ON PR.CODIGO = IT.Cod_Produto AND ES.COD_ESTABE = IT.COD_ESTABE 
 		inner join NFECB CB ON IT.COD_ESTABE = CB.COD_ESTABE  AND IT.PROTOCOLO = CB.PROTOCOLO
 	 WHERE ES.Cod_Estabe = 1
-	AND CB.SERIE = '1'
+	AND CB.STATUS IN NOT ('A', 'C')
 	AND pr.Cod_Fabricante in (158, 319, 123, 321, 588, 338, 33, 237, 164, 1022)
 	AND cb.Dat_Entrada >= '20210101'
 	AND cb.Dat_Entrada <= '20231031'
