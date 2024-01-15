e3d4b475f33b6d743ce2477943c4bc3795758358
Helena Nascimento <hnasc.1989@gmail.com>
2024-01-15 17:53:03 -0300

    'hoje 15012024'

--- C_ABC_E_CURS.sql
+++ C_ABC_E_CURS.sql
@@ -1,6 +1,6 @@
 declare 
 	@codEstab int = 1,
-	@CodFab int = 349,
+	@CodFab int = 158,
 	@DatIn smalldatetime = '20230101',
 	@DatFim smalldatetime = '20231231',
 	@CodProd int, -- = 21165,
--- C_ABC_S_CONSU.sql
+++ C_ABC_S_CONSU.sql
@@ -26,12 +26,12 @@ SELECT
 DISTINCT
 --pr.Cod_Fabricante, 
 --fb.Fantasia,
-pr.cod_ean,
-pr.codigo,
-pr.descricao,
-pc.Cod_PolCom,
-QtdVen = Sum(it.Qtd_Produto+it.Qtd_Bonificacao),    
-VlrFatVen =  Sum(it.Vlr_LiqItem-it.Vlr_RecSbt)
+--pr.cod_ean,
+--pr.codigo,
+--pr.descricao
+pc.Cod_PolCom
+--QtdVen = Sum(it.Qtd_Produto+it.Qtd_Bonificacao),    
+--VlrFatVen =  Sum(it.Vlr_LiqItem-it.Vlr_RecSbt)
 --VlrBasDscVen = Sum(it.Vlr_LiqItem-it.Vlr_SubsTrib-it.Vlr_SbtRes-it.Vlr_RecSbt-it.Vlr_SubsTribEmb-it.Vlr_DespRateada-IsNull(it.Vlr_DspExt,0)),
 --VlrBasDsc = Sum(it.Vlr_LiqItem-it.Vlr_RecSbt-it.Vlr_DespRateada-IsNull(it.Vlr_DspExt,0))
 --VlrVen1 = Sum((it.Vlr_LiqItem-it.Vlr_SubsTrib-it.Vlr_SbtRes-it.Vlr_RecSbt-it.Vlr_SubsTribEmb-it.Vlr_DespRateada-IsNull(it.Vlr_DspExt,0))*(1-IsNull(it.Per_DescontoFin,0)/100)),
@@ -44,9 +44,9 @@ FROM NFSCB cb
    left join POCOM PC on it.Id_PolCom = pc.Id_PolCom
    left join FABRI FB on pr.Cod_Fabricante = fb.codigo
 WHERE cb.Cod_Estabe = 1
-AND pr.Cod_Fabricante = 164 --in (158,319,123,321,588,338,33,237,164,1022)
+AND pr.Cod_Fabricante = 158 --in (158,319,123,321,588,338,33,237,164,1022)
 AND (cb.Status = 'F' and cb.Tip_Saida = 'V') 
-AND cb.Dat_Emissao >= '20231201' 
+AND cb.Dat_Emissao >= '20230101' 
 AND cb.Dat_Emissao <= '20231231'
 Group by 
 pr.Cod_Fabricante, 
@@ -56,4 +56,4 @@ pr.codigo,
 pr.descricao,
 pc.Cod_PolCom
 
-order by 3
+order by 1
