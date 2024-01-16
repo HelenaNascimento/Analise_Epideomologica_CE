3ffe1871f9e3b8f0e07e51115373ae8efcaec7c5
Helena Nascimento <hnasc.1989@gmail.com>
2024-01-16 17:38:28 -0300

    'hoje16012024'

--- C_ABC_E_CURS.sql
+++ C_ABC_E_CURS.sql
@@ -10,9 +10,14 @@ declare
 	@DatComp smalldatetime
 
 	Declare CursorProd CURSOR FOR 
-		SELECT DISTINCT Codigo 
+		SELECT 
+			top 1
+			Codigo,
+			Prc_Fabric20,
+			Prc_UltEnt 
 		FROM PRODU
-		WHERE Cod_Fabricante = @CodFab
+			inner join PRXE
+		WHERE Cod_Fabricante = 1 @CodFab
 
 	OPEN CursorProd
 
