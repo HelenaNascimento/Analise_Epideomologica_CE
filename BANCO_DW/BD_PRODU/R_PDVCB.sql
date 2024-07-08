14426ea02689ff6e171bce1d82c0247a93dfce96
Helena Nascimento <hnasc.1989@gmail.com>
2024-07-07 21:08:14 -0300

    'atuaç BANCO_DW'

--- BANCO_DW/BD_PRODU/R_PDVCB.sql
+++ BANCO_DW/BD_PRODU/R_PDVCB.sql
@@ -1,59 +1,122 @@
-USE [DW_PROD]
-GO
-
-/****** Object:  Table [dbo].[PDVCB_CP]    Script Date: 13/04/2024 17:26:33 ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-CREATE TABLE [dbo].[R_PDVCB](
-	[Numero] [int] NOT NULL,
-	[Cod_PedCli] [nchar](20) ,
-	[Tip_Pedido] [varchar](1) ,
-	[Cod_Cliente] [int] ,
-	[Cod_RegTri] [int] ,
-	[Cod_Vendedor] [int] ,
-	[Id_PolCom] [int] ,
-	[Cod_Prz] [int] ,
-	[Cod_Rota] [int] ,
-	[Status1] [varchar](1) ,
-	[Status2] [varchar](1) ,
-	[Dat_Pedido] [smalldatetime] ,
-	[Hor_ConferIni] [smalldatetime] ,
-	[Hor_ConferFim] [smalldatetime] ,
-	[Hor_DbqFin] [smalldatetime] ,
-	[Hor_DbqLic] [smalldatetime] ,
-	[Hor_DbqRnt] [smalldatetime] ,
-	[Hor_Entrada] [smalldatetime] ,
-	[Hor_Saida] [smalldatetime] ,
-	[Hor_Liberacao] [smalldatetime] ,
-	[Hor_Fatura] [smalldatetime] ,
-	[Hor_Fechamento] [smalldatetime] ,
-	[Hor_Cancel] [smalldatetime] ,
-	[Hor_ImpEtq] [smalldatetime] ,
-	[Hor_Prenota] [smalldatetime] ,
-	[Cod_FunSeparador] [int] ,
-	[Cod_FunEmbalador] [int] ,
-	[Cod_FunConferidor] [int] ,
-	[Cod_Digitador] [int] ,
-	[Usuario] [varchar](15) ,
-	[Nom_UsuCancel] [varchar](15) ,
-	[Nom_UsuDesbloq] [varchar](15),
-	[Nom_UsuDesbloqLic] [varchar](15) ,
-	[Nom_UsuDesbloqRnt] [varchar](15) ,
-	[Observacao] [text] ,
-	[Obs_Padrao] [text] ,
-	[Obs_NotFis] [text] ,
-	[Obs_IntFecPdv] [text] ,
-	[Obs_IntCtaRec] [text] ,
-	[C_VlrPedido] [numeric](18, 4) ,
- CONSTRAINT [PK_PDVCB_CP] PRIMARY KEY CLUSTERED 
-(
-	[Numero] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+use BD_DW
 GO
 
 
+DECLARE @BD_DW int = (SELECT COUNT(NUMERO) FROM BD_DW.dbo.R_PDVCB), 
+		@BD_REMOTE int = (SELECT COUNT(NUMERO) FROM [RemoteServerName].[DMD].[dbo].[PDVCB]),
+		@BD_REMOTE_1 int = (SELECT COUNT(NUMERO) FROM [RemoteServerName].[DMD].[dbo].[PDVCB] WHERE Cod_Estabe = 1) 
+
+--(SELECT COUNT(NUMERO) FROM PROD_2023.dbo.PDVCB)
+--(SELECT TOP 1 1 FROM [RemoteServerName].[DMD].[dbo].[PDVCB])
+
+BEGIN TRY
+	BEGIN TRANSACTION;
+	IF @BD_DW < @BD_REMOTE
+	BEGIN
+		INSERT INTO BD_DW.dbo.R_PDVCB ([Numero], [Cod_PedCli] ,[Tip_Pedido],[Cod_Cliente],[Cod_RegTri],[Cod_Vendedor],[Id_PolCom],[Cod_Prz],[Cod_Rota],[Status1],[Status2],[Dat_Pedido],[Hor_ConferIni],[Hor_ConferFim],[Hor_DbqFin],
+							[Hor_DbqLic],[Hor_DbqRnt],[Hor_Entrada],[Hor_Saida],[Hor_Liberacao],[Hor_Fatura],[Hor_Fechamento],[Hor_Cancel],[Hor_ImpEtq],[Hor_Prenota],[Cod_FunSeparador],[Cod_FunEmbalador],[Cod_FunConferidor],
+							[Cod_Digitador],[Usuario],[Nom_UsuCancel],[Nom_UsuDesbloq],[Nom_UsuDesbloqLic],[Nom_UsuDesbloqRnt],[Observacao],[Obs_Padrao],[Obs_NotFis],[Obs_IntFecPdv],[Obs_IntCtaRec],[C_VlrPedido])
+		SELECT [Numero], [Cod_PedCli] ,[Tip_Pedido],[Cod_Cliente],[Cod_RegTri],[Cod_Vendedor],[Id_PolCom],[Cod_Prz],[Cod_Rota],[Status1],[Status2],[Dat_Pedido],[Hor_ConferIni],[Hor_ConferFim],[Hor_DbqFin],
+				[Hor_DbqLic],[Hor_DbqRnt],[Hor_Entrada],[Hor_Saida],[Hor_Liberacao],[Hor_Fatura],[Hor_Fechamento],[Hor_Cancel],[Hor_ImpEtq],[Hor_Prenota],[Cod_FunSeparador],[Cod_FunEmbalador],[Cod_FunConferidor],
+				[Cod_Digitador],[Usuario],[Nom_UsuCancel],[Nom_UsuDesbloq],[Nom_UsuDesbloqLic],[Nom_UsuDesbloqRnt],[Observacao],[Obs_Padrao],[Obs_NotFis],[Obs_IntFecPdv],[Obs_IntCtaRec],[C_VlrPedido]
+		FROM [RemoteServerName].[DMD].[dbo].[PDVCB]
+		WHERE Cod_Estabe = 1
+			AND [Numero] NOT IN (SELECT [Numero] FROM BD_DW.dbo.R_PDVCB)
+	END
+
+	IF @BD_DW = @BD_REMOTE_1
+	BEGIN 
+		UPDATE R_CB 
+		SET  
+		 R_CB.[Cod_RegTri] = CB.[Cod_RegTri] 
+		,R_CB.[Cod_Vendedor] = CB.[Cod_Vendedor] 
+		,R_CB.[Id_PolCom] = CB.[Id_PolCom] 
+		,R_CB.[Cod_Prz] = CB.[Cod_Prz] 
+		,R_CB.[Cod_Rota]  = CB.[Cod_Rota]  
+		,R_CB.[Status1]  = CB.[Status1]  
+		,R_CB.[Status2] = CB.[Status2] 
+		,R_CB.[Dat_Pedido] = CB.[Dat_Pedido] 
+		,R_CB.[Hor_ConferIni]  = CB.[Hor_ConferIni]  
+		,R_CB.[Hor_ConferFim]  = CB.[Hor_ConferFim]  
+		,R_CB.[Hor_DbqFin]  = CB.[Hor_DbqFin]  
+		,R_CB.[Hor_DbqLic]  = CB.[Hor_DbqLic]  
+		,R_CB.[Hor_DbqRnt]  = CB.[Hor_DbqRnt]  
+		,R_CB.[Hor_Entrada] = CB.[Hor_Entrada] 
+		,R_CB.[Hor_Saida] = CB.[Hor_Saida] 
+		,R_CB.[Hor_Liberacao] = CB.[Hor_Liberacao] 
+		,R_CB.[Hor_Fatura] = CB.[Hor_Fatura] 
+		,R_CB.[Hor_Fechamento] = CB.[Hor_Fechamento] 
+		,R_CB.[Hor_Cancel] = CB.[Hor_Cancel] 
+		,R_CB.[Hor_ImpEtq] = CB.[Hor_ImpEtq] 
+		,R_CB.[Hor_Prenota] = CB.[Hor_Prenota] 
+		,R_CB.[Cod_FunSeparador] = CB.[Cod_FunSeparador] 
+		,R_CB.[Cod_FunEmbalador] = CB.[Cod_FunEmbalador] 
+		,R_CB.[Cod_FunConferidor] = CB.[Cod_FunConferidor] 
+		,R_CB.[Cod_Digitador] = CB.[Cod_Digitador] 
+		,R_CB.[Usuario] = CB.[Usuario] 
+		,R_CB.[Nom_UsuCancel] = CB.[Nom_UsuCancel] 
+		,R_CB.[Nom_UsuDesbloq] = CB.[Nom_UsuDesbloq] 
+		,R_CB.[Nom_UsuDesbloqLic] = CB.[Nom_UsuDesbloqLic] 
+		,R_CB.[Nom_UsuDesbloqRnt] = CB.[Nom_UsuDesbloqRnt] 
+		,R_CB.[Observacao] = CB.[Observacao] 
+		,R_CB.[Obs_Padrao]  = CB.[Obs_Padrao]  
+		,R_CB.[Obs_NotFis] = CB.[Obs_NotFis] 
+		,R_CB.[Obs_IntFecPdv]  = CB.[Obs_IntFecPdv]  
+		,R_CB.[Obs_IntCtaRec] = CB.[Obs_IntCtaRec] 
+		,R_CB.[C_VlrPedido]  = CB.[C_VlrPedido]  
+		FROM BD_DW.dbo.R_PDVCB R_CB
+			INNER JOIN [RemoteServerName].[DMD].[dbo].[PDVCB] CB ON  R_CB.[Numero] = CB.[Numero]  
+					AND R_CB.[Cod_PedCli] = CB.[Cod_PedCli] 
+					AND R_CB.[Cod_Cliente]  = CB.[Cod_Cliente] 
+				
+		WHERE 
+			CB.Cod_Estabe = 1
+		AND ( R_CB.[Cod_RegTri] <> CB.[Cod_RegTri] 
+		OR R_CB.[Cod_Vendedor] <> CB.[Cod_Vendedor] 
+		OR R_CB.[Id_PolCom] <> CB.[Id_PolCom] 
+		OR R_CB.[Cod_Prz] <> CB.[Cod_Prz] 
+		OR R_CB.[Cod_Rota] <> CB.[Cod_Rota]  
+		OR R_CB.[Status1]  <> CB.[Status1]  
+		OR R_CB.[Status2] <> CB.[Status2] 
+		OR R_CB.[Dat_Pedido] <> CB.[Dat_Pedido] 
+		OR R_CB.[Hor_ConferIni]  <> CB.[Hor_ConferIni]  
+		OR R_CB.[Hor_ConferFim]  <> CB.[Hor_ConferFim]  
+		OR R_CB.[Hor_DbqFin]  <> CB.[Hor_DbqFin]  
+		OR R_CB.[Hor_DbqLic]  <> CB.[Hor_DbqLic]  
+		OR R_CB.[Hor_DbqRnt]  <> CB.[Hor_DbqRnt]  
+		OR R_CB.[Hor_Entrada] <> CB.[Hor_Entrada] 
+		OR R_CB.[Hor_Saida] <> CB.[Hor_Saida] 
+		OR R_CB.[Hor_Liberacao] <> CB.[Hor_Liberacao] 
+		OR R_CB.[Hor_Fatura] <> CB.[Hor_Fatura] 
+		OR R_CB.[Hor_Fechamento] <> CB.[Hor_Fechamento] 
+		OR R_CB.[Hor_Cancel] <> CB.[Hor_Cancel] 
+		OR R_CB.[Hor_ImpEtq] <> CB.[Hor_ImpEtq] 
+		OR R_CB.[Hor_Prenota] <> CB.[Hor_Prenota] 
+		OR R_CB.[Cod_FunSeparador] <> CB.[Cod_FunSeparador] 
+		OR R_CB.[Cod_FunEmbalador] <> CB.[Cod_FunEmbalador] 
+		OR R_CB.[Cod_FunConferidor] <> CB.[Cod_FunConferidor] 
+		OR R_CB.[Cod_Digitador] <> CB.[Cod_Digitador] 
+		OR R_CB.[Usuario] <> CB.[Usuario] 
+		OR R_CB.[Nom_UsuCancel] <> CB.[Nom_UsuCancel] 
+		OR R_CB.[Nom_UsuDesbloq] <> CB.[Nom_UsuDesbloq] 
+		OR R_CB.[Nom_UsuDesbloqLic] <> CB.[Nom_UsuDesbloqLic] 
+		OR R_CB.[Nom_UsuDesbloqRnt] <> CB.[Nom_UsuDesbloqRnt]
+		OR R_CB.[C_VlrPedido]  <> CB.[C_VlrPedido])
+	END
+			COMMIT TRANSACTION;
+END TRY
+BEGIN CATCH
+
+    ROLLBACK TRANSACTION;
+
+    DECLARE @ErrorMessage NVARCHAR(4000);
+    DECLARE @ErrorSeverity INT;
+    DECLARE @ErrorState INT;
+
+    SELECT 
+        @ErrorMessage = ERROR_MESSAGE(),
+        @ErrorSeverity = ERROR_SEVERITY(),
+        @ErrorState = ERROR_STATE();
+
+    RAISERROR (@ErrorMessage, @ErrorSeverity, @ErrorState);
+END CATCH;
