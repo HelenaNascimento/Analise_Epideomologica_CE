e9b1a94983abfa1a59c8e4a79e4045dc0afbfa18
Helena Nascimento <hnasc.1989@gmail.com>
2025-10-17 15:22:45 -0300

    'Atualiza script de seleção de status de pedidos na NEXFAR'

--- NEXFAR/SELECT_NEXFAR_ORDER_STATUS.sql
+++ NEXFAR/SELECT_NEXFAR_ORDER_STATUS.sql
@@ -7,18 +7,10 @@ SELECT DISTINCT
 	pdv.Cod_Vendedor AS 'sellerId',
 	CASE
 		WHEN pdv.Status1 = 'P' and pdv.Status2 = 'F'  THEN 'RECEIVED_BY_DISTRIBUTOR'
-		WHEN pdv.Status1 = 'P' and pdv.Status2 = 'N'  THEN 'RECEIVED_BY_DISTRIBUTOR'
 		WHEN (pdv.Status1 = 'C' OR (pde.Num_PedVen = 0 AND pdv.Numero IS NULL)) THEN 'CANCELED'
-		WHEN (pdv.Status1 = 'D' or pdv.Status1 = 'P')  AND NF.Ret_CStat = 100 AND NF.Dat_Saida IS NULL and
-				DATEADD(day, -20, CURRENT_TIMESTAMP) < pdv.Hor_Fatura THEN 'BILLED'
-		WHEN pdv.Status1 = 'D' AND NF.Ret_CStat = 100 AND NF.Dat_Saida IS NOT NULL AND
-				Dat_RetEnt IS NULL and DATEADD(day, -20, CURRENT_TIMESTAMP) < pdv.Hor_Fatura THEN 'IN_TRANSIT'
-		WHEN pdv.Status1 = 'D' AND NF.Ret_CStat = 100 AND NF.Dat_Saida IS NOT NULL AND
-				Dat_RetEnt IS NOT NULL THEN 'DELIVERED'
-		WHEN pdv.Status1 = 'D' AND NF.Ret_CStat = 100 AND NF.Dat_Saida IS NULL and
-				DATEADD(day, -20, CURRENT_TIMESTAMP) >= pdv.Hor_Fatura THEN 'DELIVERED'
-		WHEN pdv.Status1 = 'D' AND NF.Ret_CStat = 100 AND NF.Dat_Saida IS NOT NULL AND
-				Dat_RetEnt IS NULL and DATEADD(day, -20, CURRENT_TIMESTAMP) >= pdv.Hor_Fatura THEN 'DELIVERED'
+		WHEN pdv.Status1 = 'P' and pdv.Status2 = 'N' THEN 'BILLED'
+		WHEN pdv.Status1 = 'D'AND NF.Dat_Saida IS NOT NULL AND Dat_RetEnt IS NULL  THEN 'IN_TRANSIT'
+		WHEN pdv.Status1 = 'D' AND pdv.Status2 <> 'A'  THEN 'DELIVERED'
 		END                      AS 'status',
 	CAST(pde.Dat_LeiPed AS DATE) AS 'orderDate',
 	CAST(pdv.Hor_Fatura AS DATE) AS 'billingDate'
@@ -30,8 +22,7 @@ FROM PDECB pde
             LEFT JOIN NFSCB NF ON pde.Cod_Client = NF.Cod_Cliente
     AND pdv.COD_ESTABE = NF.COD_ESTABE
     AND pdv.Cod_NumNfsIni = NF.Num_Nota
-    AND NF.Ser_Nota = '1'
---WHERE pde.Cod_PedCli IN ('176073208711142')
+--WHERE pdv.NUMERO = 51085382  
 GROUP BY pdv.Numero,
             pde.Cod_PedCli,
             pde.Num_PedVen,
@@ -45,6 +36,3 @@ GROUP BY pdv.Numero,
             Dat_RetEnt,
             pde.Dat_LeiPed,
             pdv.Hor_Fatura;
-
-
-			51083291
